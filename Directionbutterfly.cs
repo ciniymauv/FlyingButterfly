da344d3cf7961755bbe9ea5e4bf1fe2a02344c6b
mauv <myavkodes@gmail.com>
2024-08-30 12:59:52 +0300

    Lab

---
+++ Directionbutterfly.cs
@@ -0,0 +1,7 @@
+﻿namespace FlyingButterfly
+{
+    internal enum Directionbutterfly
+    {
+        Up = 1, Down = 2, Left = 3, Right = 4
+    }
+}
---
+++ Entitybutterfly.cs
@@ -0,0 +1,27 @@
+﻿namespace FlyingButterfly;
+public class Entitybutterfly
+{
+    public int Speed { get; private set; }
+
+    public double Weight { get; private set; }
+
+    public Color MainColor { get; private set; }
+
+    public Color SecondarylColor { get; private set; }
+
+    public bool Pattern { get; private set; }
+
+    public bool Antennae { get; private set; }
+
+    public double Step => Speed * 100 / Weight;
+
+    public void Init(int speed, double weight, Color mainColor, Color secondaryColor, bool pattern, bool antennae)
+    {
+        Speed = speed;
+        Weight = weight;
+        MainColor = mainColor;
+        SecondarylColor = secondaryColor;
+        Pattern = pattern;
+        Antennae = antennae;
+    }
+}
