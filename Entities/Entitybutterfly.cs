06e6c4e1222ce08869ca9b0d29015fdb603dd6c3
mauv <myavkodes@gmail.com>
2024-08-30 13:20:08 +0300

    Lab

--- Entities/Entitybutterfly.cs
+++ Entities/Entitybutterfly.cs
@@ -1,25 +1,14 @@
-﻿namespace FlyingButterfly;
-public class Entitybutterfly
+﻿namespace FlyingButterfly.Entities;
+public class Entitybutterfly : Entitysimplebutterfly
 {
-    public int Speed { get; private set; }
-
-    public double Weight { get; private set; }
-
-    public Color MainColor { get; private set; }
-
     public Color SecondarylColor { get; private set; }
 
     public bool Pattern { get; private set; }
 
     public bool Antennae { get; private set; }
 
-    public double Step => Speed * 100 / Weight;
-
     public void Init(int speed, double weight, Color mainColor, Color secondaryColor, bool pattern, bool antennae)
     {
-        Speed = speed;
-        Weight = weight;
-        MainColor = mainColor;
         SecondarylColor = secondaryColor;
         Pattern = pattern;
         Antennae = antennae;
---
+++ Entities/Entitysimplebutterfly.cs
@@ -0,0 +1,19 @@
+﻿namespace FlyingButterfly.Entities;
+
+public class Entitysimplebutterfly
+{
+    public int Speed { get; private set; }
+
+    public double Weight { get; private set; }
+
+    public Color MainColor { get; private set; }
+
+    public double Step => Speed * 100 / Weight;
+
+    public void Init(int speed, double weight, Color mainColor)
+    {
+        Speed = speed;
+        Weight = weight;
+        MainColor = mainColor;
+    }
+}
