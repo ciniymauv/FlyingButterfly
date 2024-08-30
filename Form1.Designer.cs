5197138c1e11f0eae6989768e9d9942a8710a80e
mauv <myavkodes@gmail.com>
2024-08-30 12:46:33 +0300

    Lab1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            button1.Location = new Point(280, 241);
+            button1.Location = new Point(12, 416);
             button1.Name = "button1";
-            button1.Size = new Size(196, 154);
+            button1.Size = new Size(81, 22);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
